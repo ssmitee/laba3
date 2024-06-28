28f1dbf6c64402ba602cc20ea8480d6bfea2cd8b
rinterron <144239782+ssmitee@users.noreply.github.com>
2024-06-28 09:32:50 +0200

    Add files via upload

---
+++ FuzzyNumberApp/FuzzyNumberApp/FuzzyNumber.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuzzyNumberApp
+{
+    public class FuzzyNumber
+    {
+        public double X { get; }
+        public double A1 { get; }
+        public double A2 { get; }
+
+        public FuzzyNumber(double x, double a1, double a2)
+        {
+            X = x;
+            A1 = a1;
+            A2 = a2;
+        }
+
+        public static FuzzyNumber operator +(FuzzyNumber A, FuzzyNumber B)
+        {
+            return new FuzzyNumber(
+                A.X + B.X,
+                A.A1 + B.A1,
+                A.A2 + B.A2
+            );
+        }
+
+        public static FuzzyNumber operator -(FuzzyNumber A, FuzzyNumber B)
+        {
+            return new FuzzyNumber(
+                A.X - B.X,
+                A.A1 - B.A1,
+                A.A2 - B.A2
+            );
+        }
+
+        public static FuzzyNumber operator *(FuzzyNumber A, FuzzyNumber B)
+        {
+            return new FuzzyNumber(
+                A.X * B.X,
+                A.X * B.A1 + A.A1 * B.X,
+                A.X * B.A2 + A.A2 * B.X
+            );
+        }
+
+        public FuzzyNumber Inverse()
+        {
+            if (X - A1 <= 0 || X + A2 <= 0)
+            {
+                throw new InvalidOperationException("Обратное число не существует");
+            }
+
+            return new FuzzyNumber(
+                1 / X,
+                1 / (X + A2),
+                1 / (X - A1)
+            );
+        }
+
+        public static FuzzyNumber operator /(FuzzyNumber A, FuzzyNumber B)
+        {
+            if (B.X - B.A1 <= 0 || B.X + B.A2 <= 0)
+            {
+                throw new InvalidOperationException("Деление на ноль невозможно");
+            }
+
+            return new FuzzyNumber(
+                A.X / B.X,
+                (A.X - A.A1) / (B.X + B.A2),
+                (A.X + A.A2) / (B.X - B.A1)
+            );
+        }
+
+        public override string ToString()
+        {
+            return $"({X} - {A1}, {X}, {X} + {A2})";
+        }
+    }
+}
---
+++ FuzzyNumberApp/FuzzyNumberApp/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Windows;
+
+namespace FuzzyNumberApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                double x1 = double.Parse(X1TextBox.Text);
+                double a1_1 = double.Parse(A1_1TextBox.Text);
+                double a2_1 = double.Parse(A2_1TextBox.Text);
+                FuzzyNumber fuzzy1 = new FuzzyNumber(x1, a1_1, a2_1);
+
+                double x2 = double.Parse(X2TextBox.Text);
+                double a1_2 = double.Parse(A1_2TextBox.Text);
+                double a2_2 = double.Parse(A2_2TextBox.Text);
+                FuzzyNumber fuzzy2 = new FuzzyNumber(x2, a1_2, a2_2);
+
+                FuzzyNumber result;
+                if (AdditionRadioButton.IsChecked == true)
+                {
+                    result = fuzzy1 + fuzzy2;
+                }
+                else if (SubtractionRadioButton.IsChecked == true)
+                {
+                    result = fuzzy1 - fuzzy2;
+                }
+                else if (MultiplicationRadioButton.IsChecked == true)
+                {
+                    result = fuzzy1 * fuzzy2;
+                }
+                else if (DivisionRadioButton.IsChecked == true)
+                {
+                    result = fuzzy1 / fuzzy2;
+                }
+                else
+                {
+                    MessageBox.Show("Выберите операцию", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+
+                ResultTextBlock.Text = $"Результат: {result}";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
